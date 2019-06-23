0710c6de071832585c0270981ec9b9582abf69c0
navastud <david.navastud@gmail.com>
2019-06-23 06:25:51 -0400

    Integracion de Controller Rest y Procesos Batch

---
+++ src/main/java/com/navastud/backendninja/controller/RestController.java
@@ -0,0 +1,19 @@
+package com.navastud.backendninja.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.navastud.backendninja.model.ContactModel;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("/rest")
+public class RestController {
+
+	@GetMapping("/checkrest")
+	public ResponseEntity<ContactModel> checkRest() {
+		ContactModel cm = new ContactModel(2, "Mikel", "Perez", "123241", "Madrid");
+		return new ResponseEntity<ContactModel>(cm, HttpStatus.OK);
+	}
+}
