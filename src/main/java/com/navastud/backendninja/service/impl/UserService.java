34b5b90a808d98b59c7dcb6c26b38ad2e6ed45a2
navastud <david.navastud@gmail.com>
2019-06-23 00:37:57 -0400

    Implementacion de Spring Security

---
+++ src/main/java/com/navastud/backendninja/service/impl/UserService.java
@@ -0,0 +1,44 @@
+package com.navastud.backendninja.service.impl;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.navastud.backendninja.entity.UserRole;
+import com.navastud.backendninja.repository.UserRepository;
+
+@Service("userService")
+public class UserService implements UserDetailsService {
+
+	@Autowired
+	@Qualifier("userRepository")
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		com.navastud.backendninja.entity.User user = userRepository.findByUsername(username);
+		List<GrantedAuthority> authorities = buildAuthorities(user.getUserRole());
+		return buildUser(user, authorities);
+	}
+
+	private User buildUser(com.navastud.backendninja.entity.User user, List<GrantedAuthority> authorities) {
+		return new User(user.getUsername(), user.getPassword(), user.isEnabled(), true, true, true, authorities);
+	}
+
+	private List<GrantedAuthority> buildAuthorities(Set<UserRole> userRoles) {
+
+		return userRoles.stream().map(userRole -> new SimpleGrantedAuthority(userRole.getRole()))
+				.collect(Collectors.toList());
+	}
+}
