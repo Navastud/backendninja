d43564dec9d7a1585a5b7e8904f6fdfbe97ca9aa
navastud <david.navastud@gmail.com>
2019-06-23 01:46:34 -0400

    Configuración de autorización

--- src/main/java/com/navastud/backendninja/controller/ContactController.java
+++ src/main/java/com/navastud/backendninja/controller/ContactController.java
@@ -4,6 +4,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,6 +33,7 @@ public class ContactController {
 		return "redirect:/contacts/showcontacts";
 	}
 
+	@PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
 	@GetMapping("/contactform")
 	public String redirectContactform(@RequestParam(name = "id", required = false) int id, Model model) {
 		LOG.info("METHOD: redirectContactform() -- PARAMS: id=" + id + " -- model=" + model);
